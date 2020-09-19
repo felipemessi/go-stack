3ef20def3580064e289883e2105fecf3f0a66d69
Felipe Messi <felipemessibraga@gmail.com>
2020-09-19 16:18:57 -0300

    refactor: separando as responsabilidades: routes +model + repository

--- go-barber/src/server.ts
+++ go-barber/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
-
+/*
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello, Go-Stack!'})
-})
+    return response.json({ message: 'Hello, Go-Stack!' })
+})*/
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!')
+    console.log('🚀 Server started on port 3333!')
 })
