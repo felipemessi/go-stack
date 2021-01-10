1db13faf26e10815d965d57b9b7171cd2a0e8e4f
felipemessi <felipemessibraga@gmail.com>
2021-01-10 09:42:38 -0300

    #chore: initial configs

--- go-barber/frontend/src/App.tsx
+++ go-barber/frontend/src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
 import GlobalStyle from './styles/global';
-import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <h1>Hello, World!</h1>;
     <GlobalStyle />
   </>
 );
