b874daa8059e5ee340e16b80e54b732dbea9e62d
felipemessi <felipemessibraga@gmail.com>
2021-01-10 02:42:33 -0300

    criando um projeto react

--- go-barber/backend/src/routes/users.routes.ts
+++ go-barber/backend/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer'
 import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
@@ -10,38 +11,49 @@ const usersRouter = Router()
 const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body
+  const { name, email, password } = request.body
 
-    const createUser = new CreateUserService()
+  const createUser = new CreateUserService()
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password
-    })
+  const user = await createUser.execute({
+    name,
+    email,
+    password
+  })
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json(userWithoutPassword)
-  } catch (err) {
-    return response.status(400).json({ error: err.message })
-  }
+  return response.json(userWithoutPassword)
 })
 
 usersRouter.patch(
   '/avatar', ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file)
+    const updateUserAvatar = new UpdateUserAvatarService()
 
-    return response.json({ ok: true })
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename
+    })
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword)
   }
 )
 
