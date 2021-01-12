84bb12347847bdcab95a834e82e6ec4e79ec01fd
felipemessi <felipemessibraga@gmail.com>
2021-01-12 20:34:57 -0300

    test: user services test

---
+++ go-barber/backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,42 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User dos not exists.');
+    }
+
+    await this.usersTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+export default SendForgotPasswordEmailService;
